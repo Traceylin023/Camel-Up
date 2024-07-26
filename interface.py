00d1e2a10fdae061c781127cd35f3e26cd56211e
Tracey Lin <tlin41@stuy.edu>
2024-07-26 12:28:57 -0400

    trolled more with interface

--- interface.py
+++ interface.py
@@ -1,5 +1,6 @@
 from game import *
 from player import *
+from colorama import *
 
 class Interface():
     def __init__(self, game_manager: Game, player_1: Player, player_2: Player):
@@ -9,4 +10,16 @@ class Interface():
         self.player_2 = player_2
 
     def display():
+        init()
         pass
+
+if __name__ == "__main__": 
+    print("Welcome to Camel Up!")
+    player_1_name = input("Enter Player 1 name:")
+    player_1 = Player(player_1_name)
+    player_2_name = input("Enter Player 2 name:")
+    player_2 = Player(player_2_name)
+    game = Game(player1, player2)
+
+    init()
+
