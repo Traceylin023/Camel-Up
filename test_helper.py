acfa82473fffdc0d3f418cf7c9c65a0f4dc687e6
Eben Quenneville <tonehaven@outlook.com>
2024-07-31 11:58:33 -0400

    add: misc test cases

--- test_helper.py
+++ test_helper.py
@@ -6,11 +6,6 @@ class HelperTest(unittest.TestCase):
         """Make sure we have the right number of colors"""
         self.assertEqual(len(Color), 5) # we have 5 colors: red, yellow, green, blue, and purple 
 
-    def test_camel_equality(self):
-        """Make sure camels are equal by color."""
-        self.assertEqual(Camel(Color.red), Camel(Color.red))
-        self.assertNotEqual(Camel(Color.green), Camel(Color.blue))
-
     def test_ticket_equality(self):
         self.assertEqual(BettingTicket(Color.purple, 5), BettingTicket(Color.purple, 5))
 
@@ -19,6 +14,20 @@ class HelperTest(unittest.TestCase):
 
     def test_ticket_mismatched_values(self):
         self.assertNotEqual(BettingTicket(Color.purple, 3), BettingTicket(Color.purple, 5))
+
+    def test_ticket_invalid_value(self):
+        self.assertRaises(AssertionError, BettingTicket, Color.yellow, 10)
+
+    def test_camel_get_color(self):
+        self.assertEqual(Camel(Color.purple).get_color(), Color.purple)
+
+    def test_camel_get_invalid_color(self):
+        self.assertNotEqual(Camel(Color.yellow).get_color(), Color.green)
+
+    def test_camel_equality(self):
+        """Make sure camels are equal by color."""
+        self.assertEqual(Camel(Color.red), Camel(Color.red))
+        self.assertNotEqual(Camel(Color.green), Camel(Color.blue))
 
 if __name__ == '__main__':
     unittest.main()
